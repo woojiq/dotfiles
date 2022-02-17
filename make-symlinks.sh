797ce584fa5a0ec8b4ba42f5fbe19d7fcc3c981c
Yura Shymon <97038258+woojiq@users.noreply.github.com>
2022-02-17 15:17:05 +0200

    Update make-symlinks.sh

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -17,16 +17,17 @@ echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...done"
 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
+# go to the dotfiles directory
+echo "Go to the $dir directory"
 cd $dir
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-echo "Moving any existing dotfiles from ~ to $olddir"
+echo "Moving any existing dotfiles from ~ to $olddir..."
 for file in $files; do
-    echo "Moving $file to $olddir"
+    echo "Moving .$file to $olddir"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
+    echo "...done"
 done
