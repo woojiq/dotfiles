03cac588572666d204da8de99b986e1401c91f79
woojiq <yurii.shymon@gmail.com>
2022-02-22 11:03:47 +0200

    added i3 basic config file

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vim bashrc xinitrc"    # list of files/folders to symlink in homedir
+files="vim bashrc xinitrc i3"    # list of files/folders to symlink in homedir
 
 ##########
 
